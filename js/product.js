c944ff39f923ac19e86d5f44463419051bd93ec1
Freja Fischer <frej657e@stud.kea.dk>
2023-02-06 12:13:10 +0100

    product v1 færdig

---
+++ js/product.js
@@ -0,0 +1,22 @@
+// https://kea-alt-del.dk/t7/api/products/1525
+
+fetch("https://kea-alt-del.dk/t7/api/products/1525")
+  .then((response) => response.json())
+  .then((data) => showProduct(data));
+
+function showProduct(product) {
+  console.log(product);
+  document.querySelector("h2").textContent = product.productdisplayname;
+  document.querySelector("#type_brand .subcategory").textContent = product.subcategory;
+  document.querySelector("#type_brand .brand").textContent = product.brandname;
+  document.querySelector("#price .price_change").textContent = product.price;
+  document.querySelector("#price #new_price").textContent = (product.price / 100) * product.discount;
+  document.querySelector("#price .discount").textContent = product.discount + "% discount";
+  document.querySelector("#info .color").textContent = product.basecolour;
+  document.querySelector("#info .i_number").textContent = product.id;
+
+  if (product.soldout == false) {
+    console.log("in stock");
+    document.querySelector("#soldout").classList.add("hide");
+  }
+}
